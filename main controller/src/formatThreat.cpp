1d5c37162975a8c0476cc8cc812fcab7ca29d652
AndreasRavnsbaek <aravns19@student.aau.dk>
2022-02-11 11:44:35 +0100

    Update formatThreat.cpp

--- main controller/src/formatThreat.cpp
+++ main controller/src/formatThreat.cpp
@@ -2,3 +2,59 @@
 #include <Arduino_FreeRTOS.h>
 #include <semphr.h>
 
+struct DataFormat{
+    int joystickXformat = 0;
+    int joystickYformat = 0;
+    bool deadZoneEnableX = 0;
+    bool deadZoneEnableY = 0 
+
+};
+
+DataFormat dataFormat;
+
+void setup()
+{
+	Serial.begin(9600);
+	Serial3.begin(9600);
+	dataIn_semaphore = xSemaphoreCreateMutex();
+	if(dataIn_semaphore != NULL) xSemaphoreGive(dataIn_semaphore);
+
+	xTaskCreate(TaskReadInput, "readInput",128, NULL, 1, NULL);
+
+}
+
+void loop(){
+
+}
+
+void TaskFormatData(){
+    while (true)
+    {
+        /* code */
+    }
+    
+}
+
+
+void FormatJoystickDataX(float dataJoystickX){
+    bool toReturn;
+    if(-5 < dataIn.joystickX < 5){
+        toReturn = 1;
+    }
+    else{
+        toReturn = 0;
+    }
+
+    Xformat = dataIn.joystickX 
+    return toReturn;
+}
+
+void FormatJoystickDataY(float dataJoystickY){
+
+
+    if (-5 < dataIn.joystickY < 5)
+    {
+        dataFormat.deadZoneEnabley = 1;
+    }
+    return toReturn;
+}
