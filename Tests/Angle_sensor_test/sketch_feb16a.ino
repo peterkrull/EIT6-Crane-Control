140abe7e2ef0f784d906b33627ebef61e4f7d745
simo448g <sjohan19@student.aau.dk>
2022-02-16 13:40:31 +0100

    Create sketch_feb16a.ino
    Test

---
+++ Tests/Angle_sensor_test/sketch_feb16a.ino
@@ -0,0 +1,14 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial3.begin(115200);
+  Serial.begin(115200);
+
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.print(millis());Serial.print(",");Serial.println(Serial3.parseFloat());
+}
