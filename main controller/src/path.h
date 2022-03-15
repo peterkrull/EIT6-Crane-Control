770e4e43c3a3452f29d3abd1434191a6491bfa23
LauLauridsen <lau.lauridsen@hotmail.com>
2022-03-15 14:35:04 +0100

    Update path.h

--- main controller/src/path.h
+++ main controller/src/path.h
@@ -3,54 +3,22 @@
 
 class QauyToShip{
     public:
-        void QauyToShip::update(float xPos, float yPos, float xContainer, float theta, int *xRefpoint, int*yRefpoint, int *electroMagnet);
+        void QauyToShip::update(float xPos, float yPos, float xContainer, float containerSpeed, int *xRefpoint, int*yRefpoint, int electroMagnetLED);
+        void QauyToShip::reset();
 
     private:
         int step=0;
-        float yLastPos = 0;
-        float ySpeed =1;
-        uint16_t lastTime =0;
         uint16_t failTime =0;
 };
 
-/*
-class low_pass{
-    public:
-        low_pass(float tau);
-        double update(double input);
-        double update(double input,uint32_t dtime);
-        void restart(double value);
-    private:
-        float xTau;
-        uint32_t prev_time;
-        double output_val;
-};
 
-class lead_lag{
-    public:
-        lead_lag(float a = 0, float b = 0, float k = 0);
-        double update(double input);
-        double update(double input,float dtime);
-        void restart(double value = 0);
-    private:
-        float xa,xb,xk;
-        float prev_error, prev_output;
-        uint32_t prev_time;
-};
 
-class PID{
+class ShipToQauy{
     public:
-        PID(double Kp = 0 , double Ki = 0 , double Kd = 0 , float tau = 0);
-        double update(double error);
-        double update(double error,uint32_t dtime);
-        void restart();
+        void ShipToQauy::update(float xPos, float yPos, float xContainer, float containerSpeed, int *xRefpoint, int*yRefpoint, int electroMagnetLED);
+        void ShipToQauy::reset();
+
     private:
-        double xKp, xKi, xKd;
-        low_pass lp = low_pass(0);
-        boolean dlp;
-        uint32_t prev_time;
-        double prev_error = 0;
-        double differential = 0;
-        double integral = 0;
+        int step=0;
+        uint16_t failTime =0;
 };
-*/
