e7d2c72f80af6530b4cce1297e8bf741d91a181b
LauLauridsen <lau.lauridsen@hotmail.com>
2022-03-28 08:44:20 +0200

    Test of unlinear model

---
+++ test/TestOfUnlinearModel/src/Functions.cpp
@@ -0,0 +1,99 @@
+#include <Arduino.h>
+
+bool joystickDeadZone(float dataJoystick){
+    bool toReturn;
+    float buffer = 5;
+    
+    if(511.5+buffer > dataJoystick  && dataJoystick > 511.5-buffer){
+        toReturn = 0;
+    }
+    else{
+        toReturn = 1;
+    }
+    return toReturn;
+}
+
+int joystickOutputFormat(float dataJoystick){
+    int toReturn = int(0.19844*dataJoystick+26);
+    return toReturn;
+}
+
+// Software endstops (works on the pwm value)
+int endstop(int pwm, float min, float max, float pos){
+    int pwmEndstop = 127;
+    bool dir = 0;
+    pwmEndstop = pwm;
+
+    // Check direction (used so it can move in the other direction)
+    if (pwm < 127) {
+        dir = 0;
+    }
+    else {
+        dir = 1;
+    }
+    
+    // Chek if endstop is hit
+    if (pos > max && dir == 1) {
+    pwmEndstop = 127;
+    }
+    if (pos < min && dir == 0) {
+    pwmEndstop = 127;
+    }
+
+    return pwmEndstop;
+}
+
+uint8_t currentToPwm(float current,float fric_dead, float cust_dead) {
+
+
+    //Check for correct current value
+    if(current > 10){
+        current = 10;
+    }
+    if(current < -10){
+        current = -10;
+    }
+
+    // Linear current -> pwm conversion
+    float pwm = 10.2*current+127.5;
+
+    // Add and compensate for deadzone (friction)
+    if (abs(current) < cust_dead){
+        pwm = 127.5;
+    } else if (current < 0) {
+        pwm = pwm - fric_dead;
+    } else if (current > 0) {
+        pwm = pwm + fric_dead;
+    }
+
+    // Output limiter to 10% and 90%
+    if ((uint8_t)pwm > 255*0.9) return (uint8_t)255*0.9;
+    else if ((uint8_t)pwm < 255*0.1) return (uint8_t)255*0.1;
+    else return (uint8_t)pwm;
+  
+}
+
+void turnOnElectromagnet(bool status, int LEDPin){
+    if (status == true) {
+    digitalWrite(LEDPin,HIGH);
+    Serial3.println("M1");
+  }
+
+  if(status==false){
+    digitalWrite(LEDPin,LOW);
+    Serial3.println("M0");
+  }
+}
+int pwmLinY(float pwm) {
+    float tempPWM = pwm - 3.384;
+    
+    if(tempPWM<255*0.1){
+        tempPWM = 255*0.1;
+    }
+
+    if(tempPWM>255*0.9){
+        tempPWM = 255*0.9;
+    }
+
+    return(tempPWM);
+}
---
+++ test/TestOfUnlinearModel/src/Functions.h
@@ -0,0 +1,13 @@
+#ifndef manuelFunctions_h
+#define manuelFunctions_h
+
+#include <Arduino.h>
+
+bool joystickDeadZone(float dataJoystick);
+int joystickOutputFormat(float dataJoystick);
+int endstop(int pwm, float min, float max, float pos);
+uint8_t currentToPwm(float current, float fric_dead, float cust_dead);
+void turnOnElectromagnet(bool status, int LEDPin);
+int pwmLinY(float pwm);
+
+#endif
