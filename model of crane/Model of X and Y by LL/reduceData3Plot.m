9311a1f64919373a3e18b089bbe69b2fe1e53b70
LauLauridsen <lau.lauridsen@hotmail.com>
2022-04-11 08:49:05 +0200

    Opdatering af model

---
+++ model of crane/Model of X and Y by LL/reduceData3Plot.m
@@ -0,0 +1,7 @@
+function reduceData3Plot(inputTime1,inputData1, inputTime2,inputData2, inputTime3,inputData3)
+    hold on
+    reduceDataPlot(inputTime1,inputData1);
+    reduceDataPlot(inputTime2,inputData2);
+    reduceDataPlot(inputTime3,inputData3);
+    hold off
+end
