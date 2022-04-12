aa12a71869ea849fc7261880c09aaa2ffceb7b5e
soerenLang <58945910+soerenLang@users.noreply.github.com>
2022-04-12 10:43:58 +0200

    XcontrollerDesign

--- Controller design/XthetaControllers.m
+++ Controller design/XthetaControllers.m
@@ -21,32 +21,66 @@ k_e = 0.609;
 b = k_e/r_x;
 a = M_1*l_P-M_TL*l_P;
 G_theta = s/(s^3*a + s^2*B_x*l_P+s*g*M_1+g*B_x)*b;
+
+%%
+figure(1)
+rlocus(G_theta)
+%%
+
 D_theta = (s + .16);
 
-figure(1)
+figure(235)
 rlocus(G_theta*D_theta)
 gain_theta = 18;
 D_theta = D_theta*gain_theta;
 
-CL_theta =feedback(G_theta,D_theta);
+CL_thetaDT =feedback(G_theta*D_theta,1);
+CL_thetaFB =feedback(G_theta,D_theta);
 
-%figure(2)
-%margin(G_theta*D_theta)
 
-%figure(3)
-%impulse(CL_theta)
+figure(2)
+rlocus(minreal(CL_thetaDT))
+figure(3)
+rlocus(minreal(CL_thetaFB))
+%%
 
 G_x = (l_P*s^2+ g)/s^2;
 
-%figure(4)
-%margin(CL_theta*G_x)
-
+figure(4)
+rlocus(minreal(CL_thetaFB*G_x))
 figure(5)
-rlocus(CL_theta*G_x)
-
-
-figure(7)
-rlocus(CL_theta*G_x*(s+3)*(s+4)/s)
+rlocus(minreal(CL_thetaDT*G_x))
 
 figure(6)
-step(feedback(CL_theta*G_x,1))
+margin(minreal(CL_thetaDT*G_x))
+title('Inner loop direct term controller outerloop openloop')
+
+figure(7)
+margin(minreal(CL_thetaFB*G_x))
+%title('Inner loop feedback controller outerloop openloop')
+%%
+
+D_xDT = (1.2*s+1)/(.1716*1.2*s+1);
+D_xFB = (2.414*s+1)/(.1716*2.414*s+1);
+
+figure(8)
+
+margin(minreal(CL_thetaDT*G_x*D_xDT))
+%title('Inner loop direct term controller outerloop openloop, with controller')
+
+figure(9)
+rlocus(minreal(feedback(CL_thetaDT*G_x*D_xDT,1)))
+
+
+figure(10)
+margin(minreal(CL_thetaFB*G_x*D_xFB))
+%title('Inner loop direct term controller outerloop openloop, with controller')
+
+figure(11)
+rlocus(minreal(feedback(CL_thetaFB*G_x*D_xFB,1)))
+
+%%
+D_x = s+.1;
+
+figure
+margin(minreal(CL_thetaFB*G_x*D_x))
