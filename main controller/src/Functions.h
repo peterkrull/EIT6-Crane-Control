7e0473d0a77721c21f6519feedab9cd7c146be5a
soerenLang <58945910+soerenLang@users.noreply.github.com>
2022-04-19 12:53:43 +0200

    Changes to main code related to x-controller

--- main controller/src/Functions.h
+++ main controller/src/Functions.h
@@ -7,5 +7,6 @@ bool joystickDeadZone(float dataJoystick);
 int joystickOutputFormat(float dataJoystick);
 int endstop(int pwm, float min, float max, float pos);
 uint8_t currentToPwm(double current, bool magnetSw, float xSpeed, float ySpeed, bool axis);
+uint8_t currentToPwmX(double current, float xSpeed, bool* enableXMotor);
 void turnOnElectromagnet(bool status, int LEDPin);
 #endif
