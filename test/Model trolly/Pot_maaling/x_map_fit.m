bb801d7df93845cad9384acc86d5db472dcb5bbf
JakobGammelgaard <83281635+JakobGammelgaard@users.noreply.github.com>
2022-04-22 13:07:20 +0200

    Updated plots

--- test/Model trolly/Pot_maaling/x_map_fit.m
+++ test/Model trolly/Pot_maaling/x_map_fit.m
@@ -10,13 +10,13 @@ y1 = polyval(p1,x1);
 plot(mean,in.Pos,'o',x1,y1)
 grid on
 
-text(120,3.8,'y=0.0048x-0.6765')
-title('Mean ADC-reading as function of x-position')
+text(120,3.8,'f(x)=0.0048x-0.6765')
+title('Mean ADC-reading as function of trolly position')
 set(gcf,'position',[0.4,-0.5,500,300])
 
 xlabel('Mean ADC-reading [.]')
-ylabel('X-pos [m]')
-legend('Mean ADC-reading','Linear regression')
+ylabel('Trolly position  [m]')
+legend('Mean ADC-reading','Linear regression','Location','southeast')
 %% 
 
 
