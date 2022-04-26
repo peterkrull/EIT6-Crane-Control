9545f43fcd7428c296e278684c7942baa34cda69
soerenLang <58945910+soerenLang@users.noreply.github.com>
2022-04-26 10:22:35 +0200

    Angle controller matlab

---
+++ Controller design/thetaController.m
@@ -0,0 +1,86 @@
+clear
+close all
+clc
+
+s = tf('s');
+run("x non linear model/variables.m")
+
+l_P = 1;
+b = k_ex/r_x;
+a = M_1*l_P-m_l*l_P;
+G_theta = -s/(s^3*a + s^2*B_x*l_P+s*g*M_1+g*B_x)*b;
+H_theta = -1;
+
+figure(1)
+rlocus(G_theta*H_theta)
+%export_fig("thetaPlantRLocus.pdf")
+
+D_theta = (s + .25);
+figure(2)
+rlocus(G_theta*H_theta*D_theta)
+%export_fig("thetaFeedbackRLocus1.pdf")
+
+figure(3)
+rlocus(G_theta*H_theta*D_theta)
+hold on
+pole = rlocus(G_theta*H_theta*D_theta,8);
+
+plot(real(pole), imag(pole), 'rx', 'Markersize', 10)
+hold off
+
+D_theta = D_theta*8;
+figure(4)
+pzmap(feedback(G_theta,H_theta*D_theta))
+xlim([-1.5 .1])
+ylim([-3.2 3.2])
+export_fig("thetaFBpzMap.pdf")
+
+%%
+close all
+xCurrent = 1;
+stepAngle(xCurrent, G_theta, H_theta, D_theta)
+xCurrent = 5;
+stepAngle(xCurrent, G_theta, H_theta, D_theta)
+xCurrent = 7.5;
+stepAngle(xCurrent, G_theta, H_theta, D_theta)
+xCurrent = 8;
+stepAngle(xCurrent, G_theta, H_theta, D_theta)
+xCurrent = 8.5;
+stepAngle(xCurrent, G_theta, H_theta, D_theta)
+xCurrent = 9;
+stepAngle(xCurrent, G_theta, H_theta, D_theta)
+xCurrent = 10;
+stepAngle(xCurrent, G_theta, H_theta, D_theta)
+
+
+function stepAngle(gain, G_theta, H_theta, D_theta)
+    figure
+    [y, time]  = step(feedback(G_theta, H_theta*D_theta));
+    
+    simulation = sim("angleControllerValidation.slx");
+    y = y*180/pi*gain;
+    simulation.Theta1 = simulation.Theta1*180/pi;
+    
+    plot(time,y)
+    hold on
+    plot(simulation.tout, simulation.Theta1)
+    hold off
+    xlim([0 10])
+    legend("Linear model", "Non linear model", "Location", "south east")
+    grid on
+    xlabel("Time [s]")
+    ylabel("Angle [deg]")
+    title(strcat(string(gain), " Ampere"))
+end
+
+
+function export_fig(name)
+    x0=0;
+    y0=0;
+    plotwidth=650/1.5;
+    plotHeight=400/1.5;
+    set(gcf,'position',[x0,y0,plotwidth,plotHeight])
+    
+    
+    exportgraphics(gcf,name,'ContentType','vector')
+end
--- Controller design/x non linear model/variables.m
+++ Controller design/x non linear model/variables.m
@@ -1,13 +1,14 @@
 %Simulation values
     J_l = 0.2;
     J_x = 0.13;
+    r_l = 0.05;
+    r_x = 0.075;
     g = 9.82;
     k_el = 0.71;
     k_ex = 0.609;
     m_l = 3.89;
     m_t = 0.729;
-    r_l = 0.05;
-    r_x = 0.075;
+    M_1 = m_t+m_l+J_x/r_x^2;
     B_l = 20;
     B_x = 12;
     sFrictionX=20;
