a460726596f2fe582482b33aa6cb1e69ba6c9f60
AndreasRavnsbaek <aravns19@student.aau.dk>
2022-05-05 15:06:15 +0200

    Revert "Merge branch 'main' of https://github.com/peterkrull/EIT6-Crane-Control"
    This reverts commit f9cc358b464684ddb25f26c8fba96505da855e17, reversing
    changes made to 345a18b9f06fa036deb86b67b7f2eb72d2c40085.

--- Controller design/TestOfTrolleyController/main controller/src/displayHandler.h
+++
@@ -1,6 +0,0 @@
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_I2CDevice.h>
-
-void initializeDisplay(Adafruit_SSD1306 *display);
-void displayInfo(Adafruit_SSD1306 *display,inputs in, float frequency,uint32_t *timer);
